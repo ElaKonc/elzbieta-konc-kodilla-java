8a8f94db8ec78cb28b55bffbddc3a3ef01172cf3
Elzbieta <you@example.com>
2020-03-05 08:45:47 +0100

    class SimpleUser

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -2,6 +2,16 @@ package com.kodilla.testing;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test ok");
+        } else {
+            System.out.println("error");
+        }
+
+        System.out.println("Test 2");
     }
 }
