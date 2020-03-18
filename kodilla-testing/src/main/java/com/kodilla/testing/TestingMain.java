f36a0d7feee25043bf3a173af4c2d83740d3c614
Elzbieta <you@example.com>
2020-03-18 21:49:54 +0100

    Change TestMain

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,19 +5,7 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
 
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUserName();
 
-        if(result.equals("theForumUser")) {
-            System.out.println("test ok");
-        } else {
-            System.out.println("error");
-        }
-
-        System.out.println("Test 2\n");
-
-        System.out.println(Calculator.add(5, 3));
-        System.out.println(Calculator.substract(2,6));
     }
 }
